109b448bde6ae9ee22f1bd3d1db38dd6109e5d2d
amidshojaeinasab <amidshojaeinasab@gmail.com>
2025-10-08 17:56:22 +0330

    add styles table coin

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -3,12 +3,14 @@
 import chartUp from "../../assets/chart-up.svg"
 import chartDown from "../../assets/chart-down.svg"
 
+import styles from "./TableCoin.module.css"
+
 function TableCoin({coins, isLoading}) {
     console.log(coins)
   return (
-    <div>
+    <div className={styles.container}>
       {isLoading ? (<RotatingLines strokeColor="#3874ff" strokeWidth={2}/>) : 
-      (<table>
+      (<table className={styles.table}>
        <thead>
          <tr>
           <th>Coin</th>
@@ -36,14 +38,14 @@ const TableRow = ({ coin }) => {
   return(
     <tr>
             <td>
-              <div>
+              <div className={styles.symbol}>
                 <img src={coin.image} alt="" />
                 <span>{coin.symbol.toUpperCase()}</span>
               </div>
             </td>
             <td>{coin.name}</td>
             <td>${coin.current_price.toLocaleString()}</td>
-            <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
+            <td className={coin.price_change_percentage_24h > 0 ? styles.success : styles.error}>{coin.price_change_percentage_24h.toFixed(2)}%</td>
             <td>{coin.total_volume.toLocaleString()}</td>
             <td><img src={coin.price_change_percentage_24h > 0 ? chartUp : chartDown } alt="" /></td>
     </tr>

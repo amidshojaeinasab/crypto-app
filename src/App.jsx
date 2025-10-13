3f94d9180e99291f9a175c458af05bab6d3fe34b
amidshojaeinasab <amidshojaeinasab@gmail.com>
2025-10-13 11:31:54 +0330

    add layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import HomePage from "./components/templates/HomePage";
+import Layout from "./layouts/Layout";
 
 function App() {
 
 
   return (
     <>
-      <HomePage/>
+      <Layout>
+        <HomePage/>
+      </Layout>
     </>
   )
 }
---
+++ src/layouts/Layout.jsx
@@ -0,0 +1,19 @@
+﻿import styles from "./Layout.module.css"
+function Layout({children}) {
+  return (
+    <>
+    <header className={styles.header}>
+      <h1>Crypto App</h1>
+      <p>
+        <a href="https://shojaeinasab.ir">Shojaei Nasab</a>
+      </p>
+    </header>
+    {children}
+    <footer className={styles.footer}>
+      <p>Developed by Shojaei Nasab</p>
+    </footer>
+    </>
+  )
+}
+
+export default Layout
